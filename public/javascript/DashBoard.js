634e5c62e099d7fbe260059eba4a59d816c24eb9
Victor <victor@localhost.localdomain>
2021-07-20 12:32:57 -0300

    5 -> Commit

--- public/javascript/DashBoard.js
+++ public/javascript/DashBoard.js
@@ -1,25 +1,51 @@
 let Card_1 = document.querySelector('.Card-1');
 let BtnStart = document.querySelector('#Start-Trainning');
 let ContentModal = document.querySelector('#Content-Modal');
+let ModalText = document.querySelector('.font-size-18');
 let NumbersModal = document.querySelector("#Numbers-Modal");
+let TextCenter = document.querySelector('.text-center');
+let HiddenName = document.querySelector('#HiddenName')
+let HiddenSubTitle = document.querySelector('#HiddenSubTitle');
+let References = document.querySelector('#References');
+let Chronometer = document.querySelector(".Chronometer");
+let SubTitleChronometer = document.querySelector('.SubTitle-Chronometer');
+let ChronometerTimer = document.querySelector('.Chronometer-Timer');
+let InitStartTimer = document.querySelector('#InitStart-Timer');
 
 Card_1.addEventListener('click', () => {
     window.location.href = 'http://localhost:3000/User/DashBoard#modal-5'
 });
 
-let Minute = 1
+let Minute = 0
 
-BtnStart.addEventListener('click', function TimeModal() {
-    ContentModal.style.display = 'none'
-    NumbersModal.style.display = 'block'
+BtnStart.addEventListener('click', () => {
 
-    NumbersModal.innerHTML = Minute
-    Minute = setTimeout(TimeModal, 1000)
+    ModalText.style.display = 'none'; TextCenter.style.display = 'none';
+    HiddenName.style.display = 'none'; HiddenSubTitle.style.display = 'none';
+    HiddenTitle.style.display = 'none'; References.style.display = 'none';
+    BtnStart.style.display = 'none';
+
+    if (Chronometer.style.display !== 'block') {
+        Chronometer.style.display = 'block';
+    }
+
+});
+
+InitStartTimer.addEventListener('click', function StartTimer() {
+
+    Minute += 1
+
+    if (Minute < 10) {
+        ChronometerTimer.innerHTML = '00:0' + Minute
+    }
+    else if (Minute > 10) {
+        ChronometerTimer.innerHTML = '00:' + Minute
+    }
 
     if (Minute < 60) {
-        NumbersModal.innerHTML = Minute
+        setTimeout(StartTimer, 1000)
+    } else {
+        ChronometerTimer.innerHTML = '01:00'
     }
-    else {
-        NumbersModal.innerHTML = '0'
-    }
-});
+
+})
